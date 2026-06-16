6a05c033bce6bb3f3a7a08f6501923c59e9c45d2
devfebri <devpebri@gmail.com>
2026-06-16 15:45:31 +0700

    url

--- routes/web.php
+++ routes/web.php
@@ -3,8 +3,7 @@
 use App\Http\Controllers\SensorReadingController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/monitor', [SensorReadingController::class, 'dashboard'])->name('sensor.monitor');
+Route::get('/', [SensorReadingController::class, 'dashboard'])->name('sensor.monitor');
